1a0ca0641f00e5c2cdcf5e1c2cbc9db39f269a71
mahnoorshabbir12 <tulipkardo123@gmail.com>
2025-07-13 23:12:27 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 // index.js
 // where your node app starts
 
-// init project
 var express = require('express');
+var cors = require('cors');
 var app = express();
+
+// enable CORS so that your API is remotely testable by FCC 
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+// serve static files
+app.use(express.static('public'));
+
+// homepage route
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// hello test endpoint
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+// timestamp endpoint
 app.get("/api/:date?", function (req, res) {
-  var date = req.params.date;
-  if (!date) {
+  let dateString = req.params.date;
+  let date;
+
+  if (!dateString) {
     date = new Date();
-  } else if (/^\d+$/.test(date)) {
-    date = new Date(parseInt(date));
+  } else if (!isNaN(dateString)) {
+    // Handle Unix timestamp in milliseconds
+    date = new Date(Number(dateString));
   } else {
-    date = new Date(date);
+    date = new Date(dateString);
   }
 
-  if (isNaN(date.getTime())) {
+  if (date.toString() === "Invalid Date") {
     return res.json({ error: "Invalid Date" });
   }
 
@@ -24,28 +45,7 @@ app.get("/api/:date?", function (req, res) {
   });
 });
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// Listen on port set in environment variable or default to 3000
+// start server
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
